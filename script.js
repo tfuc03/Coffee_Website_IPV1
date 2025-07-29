90e03a1371c6d6058b639f1b4f48d16d79ea133a
Tfuc03 <phamphucdev1n@gmail.com>
2025-07-29 18:23:55 +0700

    Update Testimonials Section use Swiper CSS

--- script.js
+++ script.js
@@ -7,4 +7,37 @@ menuOpenButton.addEventListener("click", () =>  {
 });
 
 //Close menu when the close button is clicked 
-menuCloseButton.addEventListener("click", () => menuOpenButton.click()); 
+menuCloseButton.addEventListener("click", () => menuOpenButton.click());
+
+// Initialize Swiper
+const swiper = new Swiper('.slider-wrapper', {
+  loop: true,
+  grabCursor: true,
+  spaceBetween: 25,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    dynamicBullets: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // Responsive Breakpoint
+  breakpoints: {
+    0: {
+        slidesPerView: 1
+    },
+    768: {
+        sldersPerView: 2
+    },
+1024 : {
+    slidesPerView: 3
+}
+  }
+});
